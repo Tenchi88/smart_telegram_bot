ef352c471303351a2f7aa0734defaa8e4c0f2799
Aleksey Vinogradov <aleksey.vinogradov@rt.ru>
2018-01-24 20:14:16 +0300

    key parser for openshift

--- helpers/openshift.py
+++ helpers/openshift.py
@@ -1,5 +1,7 @@
 from os import environ
+import os
 
+os.mkdir('~/.ssh', 400)
 k = environ['GIT_LAB_KEY']
-with open('.ssh/git_lab', 'w') as f:
+with open('~/.ssh/git_lab', 'w') as f:
     f.write(k)
