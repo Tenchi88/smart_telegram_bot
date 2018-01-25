b2d2cc202cde0beae36e628e0f687d1087fc18f7
Aleksey Vinogradov <aleksey.vinogradov@rt.ru>
2018-01-25 18:32:13 +0300

    nltk data download

--- helpers/openshift.py
+++ helpers/openshift.py
@@ -2,7 +2,7 @@ from os import environ, mkdir, path
 
 ssh_path = '/root/.ssh/'
 if not path.exists(ssh_path):
-    mkdir(ssh_path, 400)
+    mkdir(ssh_path)
 k = environ['GIT_LAB_KEY']
 with open(ssh_path + 'git_lab', 'w') as f:
     f.write(k)
